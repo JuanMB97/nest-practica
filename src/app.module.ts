160c60da1a1c2db1cdc2e6b888f2d8f6099ff0d4
JuanMB <barretojuanpmg@gmail.com>
2023-03-22 20:37:38 -0300

    Mas

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductosController } from './productos/controllers/productos.controller';
 import { ProductosService } from './productos/services/productos.service';
+import { UsuariosModule } from './usuarios/usuarios.module';
 
 @Module({
-  imports: [],
+  imports: [UsuariosModule],
   controllers: [AppController, ProductosController],
   providers: [AppService, ProductosService],
 })
---
+++ src/usuarios/entities/usuario.entity.ts
@@ -0,0 +1,6 @@
+export class Usuario {
+  id: number;
+  email: string;
+  usuario: string;
+  password: string;
+}
